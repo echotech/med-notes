f270c934e0e6144f3043ae5fa3366607c5541f36
Jed Reisner <djechotech@gmail.com>
2023-11-09 22:20:49 -0700

    system prompt

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -15,12 +15,16 @@ export async function POST(req: Request) {
   const { disease } = await req.json();
   let prompt = "What is 2+2?"
   if (typeof disease === 'string' && disease.trim().length > 0) {
-    prompt = `You are an experienced medical assistant responsible for writing SOAP notes that are concise, professional, and strictly relevant to the patient's condition. When documenting a case, unnecessary details and generic placeholders are to be omitted. Your task now is to write only the subjective, objective, assessment, disease description, disease treatment, and physical result components of a SOAP note for a patient with ${disease}. Please focus solely on this part and provide information that is directly pertinent to the diagnosis of ${disease}, including any relevant clinical findings and essential details. The note should be factual, succinct, and ready for immediate inclusion in the patient's medical record without the need for further editing.`;
+    prompt = `Your task now is to write only the subjective, objective, assessment, disease description, disease treatment, and physical result components of a SOAP note for a patient with ${disease}. Please focus solely on this part and provide information that is directly pertinent to the diagnosis of ${disease}, including any relevant clinical essential details.`;
   }
   const response = await openai.createChatCompletion({
     model: 'gpt-4-1106-preview',
     stream: true,
     messages: [
+      {
+        role: 'system',
+        content: `You are an experienced medical assistant responsible for writing SOAP notes that are concise, professional, and strictly relevant to the patient's condition. When documenting a case, unnecessary details and generic placeholders are to be omitted. Your task will be to write only the subjective, objective, assessment, disease description, disease treatment, and physical result components of a SOAP note for a patient. The note should be factual, succinct, and ready for immediate inclusion in the patient's medical record without the need for further editing.`
+      },
       {
         role: 'user',
         content: prompt
