8385a7c1026f5143100e33ba8e75ce3dca8fde22
Jed Reisner <djechotech@gmail.com>
2023-11-09 17:39:53 -0700

    first try

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -11,29 +11,18 @@ const openai = new OpenAIApi(config);
 export const runtime = 'edge';
 
 export async function POST(req: Request) {
-  const { vibe, bio } = await req.json();
+const soapComponents = ['subjective', 'objective', 'assessment and plan', 'description', 'treatment', 'physical exam'];
+  const messages = soapComponents.map((component) => ({
+    role: 'user',
+    content: `Write me a ${component} statement for a soap note for a patient with ${disease}.`
+  }));
 
-  // Ask OpenAI for a streaming completion given the prompt
   const response = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     stream: true,
-    messages: [
-      {
-        role: 'user',
-        content: `Generate 2 ${vibe} twitter biographies with no hashtags and clearly labeled "1." and "2.". ${
-          vibe === 'Funny'
-            ? "Make sure there is a joke in there and it's a little ridiculous."
-            : null
-        }
-          Make sure each generated biography is less than 160 characters, has short sentences that are found in Twitter bios, and base them on this context: ${bio}${
-          bio.slice(-1) === '.' ? '' : '.'
-        }`,
-      },
-    ],
+    messages: messages,
   });
 
-  // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
-  // Respond with the stream
   return new StreamingTextResponse(stream);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,41 +3,38 @@
 import Image from 'next/image';
 import { useRef, useState } from 'react';
 import { Toaster, toast } from 'react-hot-toast';
-import DropDown, { VibeType } from '../components/DropDown';
 import Footer from '../components/Footer';
 import Github from '../components/GitHub';
 import Header from '../components/Header';
 import { useChat } from 'ai/react';
 
 export default function Page() {
-  const [bio, setBio] = useState('');
-  const [vibe, setVibe] = useState<VibeType>('Professional');
-  const bioRef = useRef<null | HTMLDivElement>(null);
+  const [disease, setDisease] = useState('');
+  const notesRef = useRef<null | HTMLDivElement>(null);
 
-  const scrollToBios = () => {
-    if (bioRef.current !== null) {
-      bioRef.current.scrollIntoView({ behavior: 'smooth' });
+  const scrollToNotes = () => {
+    if (notesRef.current !== null) {
+      notesRef.current.scrollIntoView({ behavior: 'smooth' });
     }
   };
 
-  const { input, handleInputChange, handleSubmit, isLoading, messages } =
-    useChat({
-      body: {
-        vibe,
-        bio,
-      },
-      onResponse() {
-        scrollToBios();
-      },
-    });
+  // Update the useChat hook to use the new disease state
+  const { input, handleInputChange, handleSubmit, isLoading, messages } = useChat({
+    body: { disease }, // Only the disease is needed now
+    onResponse() {
+      scrollToNotes();
+    },
+  });
 
   const onSubmit = (e: any) => {
-    setBio(input);
-    handleSubmit(e);
+    e.preventDefault(); // Prevent the default form submission
+    handleSubmit(); // Directly call handleSubmit as we are already tracking the disease state
   };
 
-  const lastMessage = messages[messages.length - 1];
-  const generatedBios = lastMessage?.role === "assistant" ? lastMessage.content : null;
+  // Parse the response to get the generated SOAP notes
+  const generatedNotes = messages
+    .filter((message) => message.role === "assistant")
+    .map((message) => message.content);
 
   return (
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
@@ -45,7 +42,7 @@ export default function Page() {
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
         <a
           className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
-          href="https://github.com/Nutlope/twitterbio"
+          href="https://github.com/echotech/med-notes"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -53,9 +50,8 @@ export default function Page() {
           <p>Star on GitHub</p>
         </a>
         <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
-          Generate your next Twitter bio using chatGPT
+          Automate your notes with GPT.
         </h1>
-        <p className="text-slate-500 mt-5">47,118 bios generated so far.</p>
         <form className="max-w-xl w-full" onSubmit={onSubmit}>
           <div className="flex mt-10 items-center space-x-3">
             <Image
@@ -66,36 +62,28 @@ export default function Page() {
               className="mb-5 sm:mb-0"
             />
             <p className="text-left font-medium">
-              Copy your current bio{' '}
+              Enter a disease{' '}
               <span className="text-slate-500">
-                (or write a few sentences about yourself)
               </span>
               .
             </p>
           </div>
-          <textarea
-            value={input}
-            onChange={handleInputChange}
-            rows={4}
-            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
-            placeholder={
-              'e.g. Senior Developer Advocate @vercel. Tweeting about web development, AI, and React / Next.js. Writing nutlope.substack.com.'
-            }
-          />
-          <div className="flex mb-5 items-center space-x-3">
-            <Image src="/2-black.png" width={30} height={30} alt="1 icon" />
-            <p className="text-left font-medium">Select your vibe.</p>
-          </div>
-          <div className="block">
-            <DropDown vibe={vibe} setVibe={(newVibe) => setVibe(newVibe)} />
-          </div>
+            <textarea
+    value={disease}
+    onChange={(e) => setDisease(e.target.value)}
+    rows={1}
+    className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+    placeholder={'Enter a disease name...'}
+    />
+
+          
 
           {!isLoading && (
             <button
               className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
               type="submit"
             >
-              Generate your bio &rarr;
+              Generate your notes &rarr;
             </button>
           )}
           {isLoading && (
@@ -118,40 +106,34 @@ export default function Page() {
         />
         <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
         <output className="space-y-10 my-10">
-          {generatedBios && (
-            <>
-              <div>
-                <h2
-                  className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto"
-                  ref={bioRef}
+      {generatedNotes && (
+        <>
+          <div ref={notesRef}>
+            <h2 className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto">
+              Your generated notes:
+            </h2>
+          </div>
+          <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
+            {generatedNotes.map((note, index) => {
+              return (
+                <div
+                  className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
+                  onClick={() => {
+                    navigator.clipboard.writeText(note);
+                    toast(`Note ${index + 1} copied to clipboard`, {
+                      icon: '✂️',
+                    });
+                  }}
+                  key={note}
                 >
-                  Your generated bios
-                </h2>
-              </div>
-              <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
-                {generatedBios
-                  .substring(generatedBios.indexOf('1') + 3)
-                  .split('2.')
-                  .map((generatedBio) => {
-                    return (
-                      <div
-                        className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
-                        onClick={() => {
-                          navigator.clipboard.writeText(generatedBio);
-                          toast('Bio copied to clipboard', {
-                            icon: '✂️',
-                          });
-                        }}
-                        key={generatedBio}
-                      >
-                        <p>{generatedBio}</p>
-                      </div>
-                    );
-                  })}
-              </div>
-            </>
-          )}
-        </output>
+                  <p>{note}</p>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}
+    </output>
       </main>
       <Footer />
     </div>
