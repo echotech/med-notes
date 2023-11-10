765193814a14a2d79e1fa7bc1c883f7f6e3a5d33
Jed Reisner <djechotech@gmail.com>
2023-11-09 23:03:00 -0700

    renamed

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,14 +6,14 @@ export default function Header() {
     <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
       <Link href="/" className="flex space-x-3">
         <Image
-          alt="header text"
+          alt="SOAP GPT"
           src="/writingIcon.png"
           className="sm:w-12 sm:h-12 w-8 h-8"
           width={32}
           height={32}
         />
         <h1 className="sm:text-4xl text-2xl font-bold ml-2 tracking-tight">
-          gpTweet.co
+          SOAP GPT
         </h1>
       </Link>
     </header>
