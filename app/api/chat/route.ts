0dd9e35f6239582b29b4b0c49deb547b73b9a495
Jed Reisner <djechotech@gmail.com>
2023-11-09 21:27:02 -0700

    changed prompt construction again

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -13,8 +13,10 @@ export const runtime = 'edge';
 
 export async function POST(req: Request) {
   const { disease } = await req.json();
-  const prompt = `You are an experienced medical assistant responsible for writing SOAP notes that are concise, professional, and strictly relevant to the patient's condition. When documenting a case, unnecessary details and generic placeholders are to be omitted. Your task now is to write only the subjective, objective, assessment, disease description, disease treatment, and physical result components of a SOAP note for a patient with "${disease}". Please focus solely on this part and provide information that is directly pertinent to the diagnosis of "${disease}", including any relevant clinical findings and essential details. The note should be factual, succinct, and ready for immediate inclusion in the patient's medical record without the need for further editing.`
-  
+  let prompt = "What is 2+2?"
+  if (typeof disease === 'string' && disease.trim().length > 0) {
+    prompt = `You are an experienced medical assistant responsible for writing SOAP notes that are concise, professional, and strictly relevant to the patient's condition. When documenting a case, unnecessary details and generic placeholders are to be omitted. Your task now is to write only the subjective, objective, assessment, disease description, disease treatment, and physical result components of a SOAP note for a patient with ${disease}. Please focus solely on this part and provide information that is directly pertinent to the diagnosis of ${disease}, including any relevant clinical findings and essential details. The note should be factual, succinct, and ready for immediate inclusion in the patient's medical record without the need for further editing.`;
+  }
   const response = await openai.createChatCompletion({
     model: 'gpt-4-1106-preview',
     stream: true,
