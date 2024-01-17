e6f4391c48640ca6261fb215999d0002936bec99
mikerdo <156008477+mikerdo@users.noreply.github.com>
2024-01-16 22:28:59 -0600

    Update route.ts
    Changing to hospitalist

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -15,7 +15,7 @@ export async function POST(req: Request) {
   const { disease } = await req.json();
   let prompt = "What is 2+2?"
   if (typeof disease === 'string' && disease.trim().length > 0) {
-    prompt = `Your task now is to write only a SOAP note for a patient with ${disease}. Please provide information that is directly pertinent to the diagnosis of ${disease}, including any relevant clinical essential details.`;
+    prompt = `Your task now is to write only a SOAP note for a patient hospitalized with ${disease}. Please provide information that is directly pertinent to the diagnosis of ${disease}, including any relevant clinical essential details.`;
   }
   const response = await openai.createChatCompletion({
     model: 'gpt-4-1106-preview',
@@ -23,7 +23,7 @@ export async function POST(req: Request) {
     messages: [
       {
         role: 'system',
-        content: `You are an experienced medical assistant responsible for writing SOAP notes that are concise, professional, and strictly relevant to the patient's condition. When documenting a case, unnecessary details and generic placeholders are to be omitted. Your task will be to write each of the following sections in this order: the subjective, objective, physical exam, assessment, plan, disease description, disease treatment, and expected physical presentation as a result of treatment components. The note should be factual, succinct, and ready for immediate inclusion in the patient's medical record without the need for further editing. Make sure to not include specific details about the patient, only details about the disease and treatment. When you finish a section of the soap report, include a blank line before starting the next section. Also include the text "<sep />" at the end of each section.`
+        content: `You are an experienced hospitalist responsible for writing SOAP notes that are concise, professional, and strictly relevant to the patient's condition. When documenting a case, unnecessary details and generic placeholders are to be omitted. Your task will be to write each of the following sections in this order: the subjective, objective, physical exam, assessment, plan, disease description, disease treatment, and expected physical presentation as a result of treatment components. The note should be factual, succinct, and ready for immediate inclusion in the patient's medical record without the need for further editing. Make sure to not include specific details about the patient, only details about the disease and treatment. When you finish a section of the soap report, include a blank line before starting the next section. Also include the text "<sep />" at the end of each section.`
       },
       {
         role: 'user',
