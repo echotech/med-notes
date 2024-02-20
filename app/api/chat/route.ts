649974d2778773d34ba4b3fc8b9e09bb1535efca
Jed Reisner <djechotech@gmail.com>
2024-02-20 16:33:22 -0700

    naming stuff is hard 2

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -11,13 +11,11 @@ const openai = new OpenAIApi(config);
 // Set the runtime to edge for best performance
 export const runtime = 'edge';
 
-let systemMessageContent;
-
 export async function POST(req: Request) {
   const textInput = await req.text(); // Get the stringified JSON text from the request
   console.log(textInput)
-  const { disease, role } = JSON.parse(textInput); // Parse it as JSON
-  const systemMessageContent = getSystemMessage(role)
+  const { disease, physician_type } = JSON.parse(textInput); // Parse it as JSON
+  const systemMessageContent = getSystemMessage(physician_type)
   let prompt =  "Respond with: 'If you're seeing this message, you have encountered an error. Please contact the developer and tell them PROMPT_NOT_SET.'";
   if (typeof disease === 'string' && disease.trim().length > 0) {
     prompt = `Your task now is to write only a SOAP note for a patient with ${disease}. Please provide information that is directly pertinent to the diagnosis of ${disease}, including any relevant clinical essential details.`;
@@ -38,8 +36,8 @@ export async function POST(req: Request) {
   });
 
 
-function getSystemMessage(role : string): string{
-  switch (role) {
+function getSystemMessage(physician_type : string): string{
+  switch (physician_type) {
     case 'emergency_room_physician':
       return `You are an experienced emergency room physician responsible for writing SOAP notes that are concise, professional, and strictly relevant to the patient's condition. When documenting a case, unnecessary details and generic placeholders are to be omitted. Your task will be to write each of the following sections in this order: the subjective, objective, physical exam, assessment, plan, disease description, disease treatment, and expected physical presentation as a result of treatment components. The note should be factual, succinct, and ready for immediate inclusion in the patient's medical record without the need for further editing. Make sure to not include specific details about the patient, only details about the disease and treatment. When you finish a section of the soap report, include a blank line before starting the next section. Also include the text "<sep />" at the end of each section.`;
       
